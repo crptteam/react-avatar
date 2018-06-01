53e3d5e544d9fb1db2585dafe15730b61cfdcefa
Alexander Berezovsky <fantik217@gmail.com>
2018-06-01 08:54:21 +0700

    feature/update-1, version, nwb.config withl babel

--- nwb.config.js
+++ nwb.config.js
@@ -2,11 +2,26 @@ module.exports = {
   type: 'react-component',
   npm: {
     esModules: true,
-    umd: {
-      global: 'ReactAvatar',
-      externals: {
-        react: 'React'
-      }
-    }
+    umd: false
+  },
+  babel: {
+    "plugins": [
+      "transform-class-properties",
+      "transform-es2015-destructuring",
+      "transform-object-rest-spread",
+      "transform-inline-environment-variables",
+      "transform-es2015-modules-commonjs",
+      [
+        "styled-components",
+        {
+          "ssr": false,
+          "minify": false,
+          "displayName": true,
+          "preprocess": false,
+          "transpileTemplateLiterals": false
+        }
+      ],
+      "inline-react-svg"
+    ]
   }
 }
